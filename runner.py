5f15378f8c8645dec0ad91e77b505337a90db434
twinkll <sisodia.t@husky.neu.edu>
2020-08-23 18:35:22 -0400

    Add rules for count and failed-count

--- runner.py
+++ runner.py
@@ -30,3 +30,27 @@ parser.add_argument('-c', '--count',
 parser.add_argument('-fc', '--failed-count', 
                      action='store', 
                      required=True) 
+
+args = parser.parse_args()
+
+#add rules
+if (args.count>1):
+      print("Count -",sys.argv)
+      exitCode = os.system(f'ping -c {args.count} google.com')
+      if exitCode == 0:
+          print("Success")
+          print("Tracing Successful execution --------------------------------------------------------\n")
+          pingParsing = os.system(f'pingparsing google.com -c {args.count}')
+          print(pingParsing)
+      else: 
+          while args.failed >= 2:
+            exitCode = os.system(f'ping -c {args.count} google.com')
+          print("ExitCode ----- \n",exitCode)
+          
+          print("Tracing memory usage of failed execution -----------------------------------------------------------\n")
+          sysTrace = os.system(f'strace ping -c {args.count} google.com')
+          print("/n/n/n")
+          print("Tracing Failed execution -----------------------------------------------------------\n")
+          pingParsing = os.system(f'pingparsing google.com -c {args.count}')
+          print(sysTrace)
+          print(pingParsing)
